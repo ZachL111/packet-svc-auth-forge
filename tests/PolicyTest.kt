5196c577b9167ff1d70626d3cc294a906f52ab2a
ZachL111 <dog3miner@gmail.com>
2026-05-05 15:16:33 -0700

    Initial commit

---
+++ tests/PolicyTest.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val signalcase_1 = Signal(86, 79, 23, 11, 9)
+    check(Policy.score(signalcase_1) == 138)
+    check(Policy.classify(signalcase_1) == "review")
+    val signalcase_2 = Signal(68, 98, 10, 20, 4)
+    check(Policy.score(signalcase_2) == 94)
+    check(Policy.classify(signalcase_2) == "review")
+    val signalcase_3 = Signal(95, 91, 9, 14, 13)
+    check(Policy.score(signalcase_3) == 226)
+    check(Policy.classify(signalcase_3) == "accept")
+}
